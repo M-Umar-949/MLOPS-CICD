ae402e1024245d6bac6327727746247730a45d77
mtalal517 <mtalalqureshi5@gmail.com>
2025-03-04 19:01:11 +0500

    Quality Test

--- app.py
+++ app.py
@@ -1,15 +1,11 @@
-from flask import Flask, request, render_template,pickle
-
-#import pickle
+from flask import Flask, request, render_template
+import pickle
 import numpy as np
 
 app = Flask(__name__)
 
-
-
-
 # Load the trained model
-with open("iris_model.pkl","rb") as file:
+with open("iris_model.pkl", "rb") as file:
     model = pickle.load(file)
 
 # Map numeric predictions to flower names
