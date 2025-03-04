c3863b74237caeef193bde9a31cd3977e15c0bc3
M-Umar-949 <umarrajput930@gmail.com>
2025-03-04 10:48:24 +0500

    Testing

--- test_model.py
+++ test_model.py
@@ -1,13 +1,17 @@
 import unittest
-import app
 import numpy as np
+import pickle
+
+# Load the trained model (replace 'model.pkl' with your actual model file)
+with open("iris_model.pkl", "rb") as file:
+    model = pickle.load(file)
 
 
 class TestIrisModel(unittest.TestCase):
     def test_model_prediction(self):
         """Test if the model correctly predicts a valid class label."""
         sample_input = np.array([[5.1, 3.5, 1.4, 0.2]])
-        prediction = app.model.predict(sample_input)[0]
+        prediction = model.predict(sample_input)[0]
         self.assertIn(prediction, [0, 1, 2])  # Should return a valid class
 
 
