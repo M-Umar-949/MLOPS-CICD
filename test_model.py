db13b40840a9e75b8df91d3877d9238602e9841b
M-Umar-949 <umarrajput930@gmail.com>
2025-03-04 11:59:17 +0500

    Test case success

--- test_model.py
+++ test_model.py
@@ -11,7 +11,7 @@ class TestIrisModel(unittest.TestCase):
     def test_model_prediction(self):
         """Test if the model correctly predicts a valid class label."""
         sample_input = np.array([[5.1, 3.5, 1.4, 0.2]])
-        prediction = model.predict(sample_input)[1]
+        prediction = model.predict(sample_input)[0]
         self.assertIn(prediction, [0, 1, 2])  # Should return a valid class
 
 
